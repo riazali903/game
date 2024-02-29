71cde1186652179fd5e79277bf0bcc03a281a6ae
riazali903 <riazali903@hotmail.com>
2024-02-29 23:45:20 +0000

    changed how weapon class is implemented in boss class

--- boss.py
+++ boss.py
@@ -5,7 +5,7 @@ class Boss(Player):
     def __init__(self,name,weapon=Weapon('kk',20,15),health=int(100),ammo=int(50),power="Super Strength"):
         super().__init__(name,health=int(100),ammo=int(50))
         self.power = power
-        self.weapon = weapon
+        self.weapon = Weapon('KK', 30, 20)
 
     def user_power(self):
         """ Display a message indicating the boss is using its special power"""
